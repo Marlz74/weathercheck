245bbb25d7e93c179f9ce4d0bae91aeabc552132
SD <sdmarlz74@gmail.com>
2024-07-02 11:40:58 +0100

    push, waiting for domain to come live

---
+++ api/hello.php
@@ -0,0 +1,29 @@
+<?php
+
+$client_ip = $_SERVER['REMOTE_ADDR'];
+
+$name=isset($_GET['visitor_name'])?$_GET['visitor_name']:'anonymous';
+function getLocation($ip)
+{
+    $response = file_get_contents("http://ipinfo.io/" . $ip . "/json");
+    $locationData = json_decode($response, true);
+    return isset($locationData['city']) ? $locationData['city'] : 'New york';
+}
+
+function getTemperature($city)
+{
+    $response = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=9e7727ecff6cd28af7a936f871538950");
+    $weatherData = json_decode($response, true);
+
+    return isset($weatherData['main']['temp']) ? $weatherData['main']['temp'] : null;
+}
+$location = getLocation($client_ip);
+$temperature = (getTemperature($location));
+
+echo json_encode(
+    [
+        "client_ip" => $client_ip, // The IP address of the requester
+        "location" => $location, // The city of the requester
+        "greeting" => "Hello, " . ucfirst(strtolower($name)) . "!, the temperature is  $temperature  degrees Celcius in  $location "
+    ]
+);
