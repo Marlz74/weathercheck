4fbd033359a8e753c6ec38a96e9475fa70acb67a
SD <sdmarlz74@gmail.com>
2024-07-03 13:03:14 +0100

    updated

--- api/hello.php
+++ api/hello.php
@@ -1,29 +1,48 @@
 <?php
 
-$client_ip = '8.8.8.8'; //$_SERVER['REMOTE_ADDR'];
+$client_ip = getIp();
+
+function getIp(){
+    $ipAddress='';
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    
+    else {
+        $ipAddress = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ipAddress = explode(',', $ipAddress)[0];
+}
+// $client_ip='8.8.8.8';
 
 $name=isset($_GET['visitor_name'])?$_GET['visitor_name']:'anonymous';
 function getLocation($ip)
 {
     $response = file_get_contents("http://ipinfo.io/" . $ip . "/json");
-    $locationData = json_decode($response, true);
-    return isset($locationData['city']) ? $locationData['city'] : 'New york';
+    return json_decode($response, true);
 }
 
-function getTemperature($city)
+
+function getTemperature($data)
 {
-    $response = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=9e7727ecff6cd28af7a936f871538950");
+    $d=explode(',',$data['loc']);$latitude=$d[0];$longitude=$d[1];
+    $response = file_get_contents("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&current_weather=true");
     $weatherData = json_decode($response, true);
-
-    return isset($weatherData['main']['temp']) ? $weatherData['main']['temp'] : null;
+    return ($weatherData['current_weather']['temperature']);
 }
-$location = getLocation($client_ip);
-$temperature = (getTemperature($location));
+$locationData = getLocation($client_ip);
+
+$temperature = (getTemperature($locationData));
 header('content-type:application/json');
 echo json_encode(
     [
         "client_ip" => $client_ip, 
-        "location" => $location, 
-        "greeting" => "Hello, " . ucfirst(strtolower($name)) . "!, the temperature is  $temperature  degrees Celcius in  $location "
+        "location" => $locationData['city'], 
+        "greeting" => "Hello, " . ucfirst(strtolower($name)) . "!, the temperature is  $temperature  degrees Celcius in  {$locationData['city']}"
     ]
 );
